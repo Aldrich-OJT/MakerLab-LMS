02f1a31a2df562a0f2d7b56babe15a1e58ba7a27
Franklin2 <tristan456_6@live.com>
2024-03-19 08:21:35 +0800

    fixed some imports

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,37 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import HomePage from './screens/HomePage';
-import {Login, Signup} from './screens/auths/Login';
-import { NavigationContainer} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { PaperProvider } from 'react-native-paper';
+  import React from 'react';
+  import { StatusBar } from 'expo-status-bar';
+  import { StyleSheet, Text, View } from 'react-native';
+  import HomePage from './screens/HomePage';
+  import {Login, Signup} from './screens/auths/Login';
+  import { NavigationContainer} from '@react-navigation/native';
+  import { createNativeStackNavigator } from '@react-navigation/native-stack';
+  import { PaperProvider } from 'react-native-paper';
+  import { useState } from 'react';
 
-const Stack = createNativeStackNavigator()
+  const Stack = createNativeStackNavigator()
+    
 
-const authStack = () => {
-  return(
-    <Stack.Navigator>
-      <Stack.Screen name='Login' component={Login}/>
-      <Stack.Screen name='Signup' component={Signup}/>
-    </Stack.Navigator>
-  )
-}
-const AuthenticatedStack = ()=>{
-  return(
-    <Stack.Navigator>
-      <Stack.Screen name='Homepage' component={HomePage}/>
-    </Stack.Navigator>
-  )
-}
+  const authStack = () => {
+    return(
+      <Stack.Navigator>
+        <Stack.Screen name='Login' component={Login}/>
+        <Stack.Screen name='Signup' component={Signup}/>
+      </Stack.Navigator>
+    )
+  }
+  const AuthenticatedStack = ()=>{
+    return(
+      <Stack.Navigator>
+        <Stack.Screen name='Homepage' component={HomePage}/>
+      </Stack.Navigator>
+    )
+  }
 
-export default function App() {
-  return (
-    <PaperProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          {/* <Stack.Screen name="Home" component={HomePage} /> */}
-        </Stack.Navigator>
-      </NavigationContainer>
-    </PaperProvider>
-  );
-}
+  export default function App() {
+    const [isloggedIn, SetisLoggedIn] = useState(false)
+    return (
+      <PaperProvider>
+        <NavigationContainer>
+            {isloggedIn ? <authStack/> : <AuthenticatedStack/>}
+        </NavigationContainer>
+      </PaperProvider>
+    );
+  }
