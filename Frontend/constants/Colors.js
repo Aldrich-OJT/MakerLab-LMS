4dd4199ffe323dcba19ef4924152835f36ca3cb0
Franklin2 <tristan456_6@live.com>
2024-03-26 12:19:34 +0800

    assess

--- Frontend/constants/Colors.js
+++ Frontend/constants/Colors.js
@@ -1,9 +1,10 @@
 const Colors = {
     bgYellow: '#ffc42c',
+    bgDarkYellow: '#FFA800',
     bgError: "#640233",
     bgOffWhite: '#EDEDED',
     bgViolet: "#7E3290",
-    bgDarkViolet: "#9300FF",
+    bgDarkViolet: "#733AD1",
     bgGray: "#898989"
 }
 
--- Frontend/screens/Assess.jsx
+++ Frontend/screens/Assess.jsx
@@ -1,17 +1,47 @@
-import { View, StyleSheet, Text, Pressable } from "react-native";
-import Header from "../components/Header";
+import { View, StyleSheet, Text, Dimensions, ImageBackground, Image, Pressable } from "react-native";
+import Colors from "../constants/Colors";
+
+const dimensions = Dimensions.get('window');   
+const devicewidth = dimensions.width;
+const deviceheight = dimensions.height;
 
 export default function Assess() {
     return(
         <View style={styles.mainContainer}>
-            
-            <View>
-                <Text>UDDfasdfadsfsa</Text>
-            </View>
+            <ImageBackground source={require('../assets/assess-background.png')} style={styles.bgimage}>
+            <Image source={require('../assets/logo-light.png')} style={styles.logo}/>
+            <View style={styles.quizcontainer}>
+                <Text>Quiz Name</Text>
+                <Text>Description</Text>
+                <View style={styles.itemcontainer}>
+                    <View style={styles.questioncontainer}>
+                        <Text style={styles.questiontext}>1. Lorem ipsum dolor sit amet?</Text>
+                    </View>
 
-            <View>
-                
+                    <View style={styles.choicescontainer}>
+                        <View style={styles.choicesrow}>
+                            <Pressable style={styles.choices}>
+                                <Text style={styles.choicestext}>a. Nulla Vitae</Text>
+                            </Pressable>
+
+                            <Pressable style={styles.choices}>
+                                <Text style={styles.choicestext}>b. Nulla Vitae</Text>
+                            </Pressable>
+                        </View>
+
+                        <View style={styles.choicesrow}>
+                            <Pressable style={styles.choices}>
+                                <Text style={styles.choicestext}>c. Nulla Vitae</Text>
+                            </Pressable>
+
+                            <Pressable style={styles.choices}>
+                                <Text style={styles.choicestext}>d. Nulla Vitae</Text>
+                            </Pressable>
+                        </View>
+                    </View>
+                </View>
             </View>
+            </ImageBackground>
         </View>
     )
 }
@@ -23,4 +53,61 @@ const styles = StyleSheet.create({
         justifyContent:"center",
         alignItems:"center",
     },
+    bgimage: {
+        height:"100%",
+        width:"100%",
+        resizeMode: "contain",
+        justifyContent:"center",
+        alignItems:"center",
+      },
+      logo: {
+        height: 30,
+        width: 100,
+        alignSelf: 'flex-end',
+        margin: 10,
+      },
+      quizcontainer: {
+        backgroundColor: Colors.bgDarkViolet,
+        height: '85%',
+        width: '95%',
+        borderRadius: 10,
+        padding: 15,
+      },
+      itemcontainer: {
+        backgroundColor: Colors.bgYellow,
+        borderWidth: 2,
+        borderColor: 'black',
+        height: '40%',
+        width: '100%',
+      },
+      questioncontainer: {
+        borderBottomWidth: 2,
+        padding: 15,
+      },
+      questiontext:{
+        fontSize: 20,
+      },
+      choicescontainer: {
+        flexDirection: 'column',
+        gap: 20,
+        height: '60%',
+        width: '100%',
+        backgroundColor: 'red',
+        justifyContent:"center",
+        alignItems:"center",
+      },
+      choicesrow:{
+        flexDirection: 'row',
+        gap: 20,
+      },
+      choices: {
+        backgroundColor: Colors.bgDarkYellow,
+        borderRadius: 10,
+        borderColor: 'black',
+        borderWidth: 1,
+        width: '40%'
+      },
+      choicestext: {
+        fontSize: 17,
+      },
 })
